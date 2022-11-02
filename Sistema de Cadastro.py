b5e9709a9185e30b2c8996092b5c8efed6f7a398
Luiz Henrique Serafim <luizhserafim2001@hotmail.com>
2022-11-02 01:56:42 -0300

    Adicionei o arquivo

---
+++ Sistema de Cadastro.py
@@ -0,0 +1,7 @@
+Nome = input        ('Qual é o seu nome?\n')
+Idade = input       ("Qual a sua idade?\n")
+Endereço = input    ("Qual o seu endereço?\n")
+Peso = input        ("Qual o seu peso?\n")
+Altura = input      ("Qual a sua altura?\n")
+
+print ("Ficha Tecnica:\nNome:  " + Nome,Idade,Endereço,Peso,Altura)
